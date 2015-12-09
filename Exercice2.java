fe0481e6b9fe8f794d368a25697a0d829470fe74
Karin Touchie <ktouchie@gmail.com>
2015-12-09 11:34:58 +0100

    Add third variable prompt and embed extra ternary operations in original

--- Exercice2.java
+++ Exercice2.java
@@ -7,9 +7,11 @@ class Exercice2 {
         int num1 = sc.nextInt();
         System.out.println("Veuillez saisir un autre nombre:");
         int num2 = sc.nextInt();
-        int max = (num1 >= num2) ? num1 : num2;
+        System.out.println("Veuillez saisir encore un autre nombre:");
+        int num3 = sc.nextInt();
+        int max = (num1 >= num2) ? ((num1 >= num3) ? num1 : num3) : (num2 >= num3) ? num2 : num3;
 
-        System.out.println("Le maximum de ces deux nombres est " + max);
+        System.out.println("Le maximum de ces trois nombres est " + max);
         
 	}
 
